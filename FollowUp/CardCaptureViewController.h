77a456a7ef97e7c57c35706c2fb37a9df8a1ce08
Brian Soule <briansoule4@gmail.com>
2013-10-19 13:54:17 -0700

    presenting image capture view

---
+++ FollowUp/CardCaptureViewController.h
@@ -0,0 +1,13 @@
+//
+//  CardCaptureViewController.h
+//  FollowUp
+//
+//  Created by Brian Soule on 10/19/13.
+//  Copyright (c) 2013 Soule Mobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardCaptureViewController : UIViewController <UIImagePickerControllerDelegate, UINavigationControllerDelegate>
+
+@end
---
+++ FollowUp/CardCaptureViewController.m
@@ -0,0 +1,73 @@
+//
+//  CardCaptureViewController.m
+//  FollowUp
+//
+//  Created by Brian Soule on 10/19/13.
+//  Copyright (c) 2013 Soule Mobile. All rights reserved.
+//
+
+#import "CardCaptureViewController.h"
+
+@interface CardCaptureViewController ()
+
+@end
+
+@implementation CardCaptureViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+    [self startCameraControllerFromViewController: self
+                                    usingDelegate: self];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Camera Presentation
+
+
+- (BOOL) startCameraControllerFromViewController: (UIViewController*) controller
+                                   usingDelegate: (id <UIImagePickerControllerDelegate,
+                                                   UINavigationControllerDelegate>) delegate {
+    
+    if (([UIImagePickerController isSourceTypeAvailable:
+          UIImagePickerControllerSourceTypeCamera] == NO)
+        || (delegate == nil)
+        || (controller == nil))
+        return NO;
+    
+    
+    UIImagePickerController *cameraUI = [[UIImagePickerController alloc] init];
+    cameraUI.sourceType = UIImagePickerControllerSourceTypeCamera;
+    
+    // Displays a control that allows the user to choose picture or
+    // movie capture, if both are available:
+    cameraUI.mediaTypes =
+    [UIImagePickerController availableMediaTypesForSourceType:
+     UIImagePickerControllerSourceTypeCamera];
+    
+    // Hides the controls for moving & scaling pictures, or for
+    // trimming movies. To instead show the controls, use YES.
+    cameraUI.allowsEditing = NO;
+    
+    cameraUI.delegate = delegate;
+    
+    [controller presentModalViewController: cameraUI animated: YES];
+    return YES;
+}
+
+@end
